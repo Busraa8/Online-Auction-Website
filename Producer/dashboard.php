c82eed87c0f7f18d18543b7522b8db1084213ca7
Büşra Sarıgeyik <79912293+Busraa8@users.noreply.github.com>
2024-05-01 22:21:25 +0300

    dashboard.php

---
+++ Producer/dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header and Sidebar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Header</h1>
+        <div class="user-icon">
+            <img src="https://cdn-icons-png.freepik.com/512/1144/1144760.png" alt="User Icon"> 
+        </div>
+    </header>
+    <div class="container">
+        <aside>
+            <h2>Sidebar</h2>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+        </aside>
+        <main>
+            <h2>Main Content</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium reprehenderit consequatur asperiores at dolorum delectus numquam ex et vel, iste odit excepturi suscipit aliquid officia perspiciatis facere nulla alias?</p>
+        </main>
+    </div>
+</body>
+</html>
+
--- login_producer.php
+++ login_producer.php
@@ -9,17 +9,16 @@ if (isset($_POST['login'])) {
     $sql = "SELECT * FROM user_table WHERE email = '$userEmail' AND password = '$encryptedPass'";
     $result = $conn->query($sql);
 
-    
     // Sonuçları kontrol etme
     if ($result->num_rows > 0) {
         // Kullanıcı bulundu
         $row = $result->fetch_assoc();
         $role = $row["role"];
         $id = $row["id"];
-        $_SESSION["userid"] = 2;
+        $_SESSION["userid"] = $id; // Kullanıcı kimliğini sakla
         // Kullanıcının rolüne göre yönlendirme yapma
         if ($role == "producer") {
-            header("location: index_producer.php");
+            header("location: Producer/dashboard.php"); 
         } else {
             echo "Undefined user role";
         }
